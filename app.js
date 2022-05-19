8db131fe0516f00c1cf4cd03d53683de51a374c4
HugoHerreraH <launchx04415@innovaccion.mx>
2022-05-18 21:49:09 -0500

    Metodo get con id

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ app.get('/v1/explorers', (req, res) =>{
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers Get request ${new Date()}`);
+    console.log(`Getting  explorer with id ${req.params.id}`);
+    const explorer = {id: 1, name: "Hugo"}
+    res.status(200).json(explorer)
+})
+// app.get('/v1/explorers/:id', (req, res) => {
+//     console.log(`Api Explorers Get request ${new Date()}`);
+//     const explorer = {id:1, name: "Hugo"}
+//     res.status(200).json(explorer)
+// })
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
